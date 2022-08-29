2d0dc734d93fa34a1db102c54d59570400a5cd8d
Abhijeet Satpute <abhi111sat@gmail.com>
2022-08-29 11:36:44 +0530

    File uploading with multer

--- index.js
+++ index.js
@@ -2,16 +2,31 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const multer = require('multer');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/uploads');
+    },
+    filename: (req, file, cb) => {
+        cb(null, 'dec' + '-' + file.originalname);
+    }
+});
+
 app.use(express.static(path.join(__dirname, './public')));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer({storage : fileStorage}).single('file'));
 
 app.get('/', (req, res, next) => {
     res.render('index')
+})
+
+app.post('/upload', (req, res, next) => {
+    console.log(req.file);
 })
 
 app.listen(80, ()=> {
